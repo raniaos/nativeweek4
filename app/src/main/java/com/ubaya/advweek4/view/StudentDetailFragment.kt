666b73ba110fe52e79710cc0a07f22e4636ce24d
rania ori sumargo <raniaos2001@gmail.com>
2022-03-04 21:04:54 +0700

    homework week 4

---
+++ app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -0,0 +1,44 @@
+package com.ubaya.advweek4.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ubaya.advweek4.R
+import com.ubaya.advweek4.viewmodel.DetailViewModel
+import com.ubaya.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
+
+class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
+    }
+
+    fun observeViewModel() {
+        viewModel.studentLiveData.observe(viewLifecycleOwner) {
+            with (it) {
+                editID.setText(id)
+                editName.setText(name)
+                editDOB.setText(dob)
+                editPhone.setText(phone)
+            }
+        }
+    }
+}
